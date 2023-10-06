2b091519a17688de962a8e8caaa6326847969fd1
Michael Amuah <mkdamuah@hotmail.com>
2023-10-06 00:56:09 +0000

    mini update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,6 +9,13 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened and 
     returns True if all boxes can be opened, else False
     """
+    if type(boxes) is not list:
+        raise TypeError("Input must be a list.")
+    
+    for box in boxes:
+        if type(box) is not list:
+            raise TypeError("Each element in the list must be a list.")
+
     n = len(boxes)
     visited = [False] * n
     stack = [0]  # Start with the first box, which is unlocked
