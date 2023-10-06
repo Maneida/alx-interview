9c082bf0a20dc310dad70d8c2a94f88b3367ae26
Michael Amuah <mkdamuah@hotmail.com>
2023-10-06 00:46:43 +0000

    bulk commit

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines a function canUnlockAll to determine if all boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened and 
+    returns True if all boxes can be opened, else False
+    """
+    n = len(boxes)
+    visited = [False] * n
+    stack = [0]  # Start with the first box, which is unlocked
+
+    while stack:
+        box = stack.pop()
+        visited[box] = True
+
+        for key in boxes[box]:
+            if not visited[key]:
+                stack.append(key)
+
+    return all(visited)
