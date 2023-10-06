dff8baec8215530169f40e6f9fadc4f86f5fa1f3
Michael Amuah <mkdamuah@hotmail.com>
2023-10-06 00:57:37 +0000

    pycodestyle corrections

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,12 +6,12 @@ Defines a function canUnlockAll to determine if all boxes can be opened.
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be opened and 
+    Determines if all boxes can be opened and
     returns True if all boxes can be opened, else False
     """
     if type(boxes) is not list:
         raise TypeError("Input must be a list.")
-    
+
     for box in boxes:
         if type(box) is not list:
             raise TypeError("Each element in the list must be a list.")
