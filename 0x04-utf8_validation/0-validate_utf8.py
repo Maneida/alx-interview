2f87efd308faaa42c7f1cc680145fddfbfa6080b
Michael Amuah <mkdamuah@hotmail.com>
2023-10-29 23:59:38 +0000

    revert

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,29 +9,29 @@ def validUTF8(data):
     Validates if a list of integers represents a valid UTF-8 encoding.
 
     Args:
-        data (list[int]): List of integers representing 1-num data.
+        data (list[int]): List of integers representing 1-byte data.
 
     Returns:
         bool: True for valid UTF-8, else False.
     """
-    expected_bytes = 0
+    num_bytes = 0
 
     for num in data:
-        if expected_bytes == 0:
-            if num >> 7 == 0b0:
-                continue
-            elif num >> 5 == 0b110:
-                expected_bytes = 1
-            elif num >> 4 == 0b1110:
-                expected_bytes = 2
-            elif num >> 3 == 0b11110:
-                expected_bytes = 3
+        if num_bytes == 0:
+            if (num >> 3) == 0b11110:
+                num_bytes = 3
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 7) == 0b0:
+                num_bytes = 0
             else:
                 return False
         else:
-            if num >> 6 == 0b10:
-                expected_bytes -= 1
+            if (num >> 6) == 0b10:
+                num_bytes -= 1
             else:
                 return False
 
-    return expected_bytes == 0
+    return num_bytes == 0
