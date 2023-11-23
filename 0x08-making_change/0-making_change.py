2e4315a76a6091a7a28a84c2a292cb05642c5a2f
Michael Amuah <mkdamuah@hotmail.com>
2023-11-23 23:38:25 +0000

    solved i think

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module for make change function
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet the given total amount.
+
+    :param coins: List of coin values.
+    :param total: The total amount to be achieved.
+    :return: Fewest number of coins needed. If total is 0 or less, return 0.
+             If total cannot be met, return -1.
+    """
+    if total < 0:
+        return 0
+    if total == 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
